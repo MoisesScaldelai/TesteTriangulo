cfc6bb4da15629d595aa1720bfcdaef4f667ef9e
MoisesScaldelai <10770149@mackenzista.com.br>
2026-03-22 23:33:14 -0300

    Update triangulo2.py

--- triangulo2.py
+++ triangulo2.py
@@ -1,1 +1,9 @@
-
+def triangulo(n1, n2, n3):
+    if n1 == n2 and n2 == n3:
+        return 1
+    elif n1 >= n2 + n3 or n2 >= n1 + n3 or n3 >= n1 + n2:
+        return 3
+    elif n1 == n2 or n1 == n3:
+        return 0
+    else:
+        return 2
