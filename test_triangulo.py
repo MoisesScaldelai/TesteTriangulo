c81c46717872cd1ac43e0215e79ab6ca0d151f0c
MoisesScaldelai <10770149@mackenzista.com.br>
2026-03-22 23:32:14 -0300

    Update test_triangulo.py

--- test_triangulo.py
+++ test_triangulo.py
@@ -1,1 +1,25 @@
+from triangulo2 import triangulo
 
+def test1():
+    assert triangulo(2, 2, 4) == 3
+
+def test2():
+    assert triangulo(2, 2, 3) == 0
+
+def test3():
+    assert triangulo(4, 4, 4) == 1
+
+def test4():
+    assert triangulo(3, 2, 4) == 2
+
+def test5():
+    assert triangulo(7, 2, 3) == 3
+
+def test6():
+    assert triangulo(3, 3, 1) == 0
+
+def test7():
+    assert triangulo(10, 10, 10) == 1
+
+def test8():
+    assert triangulo(5, 8, 10) == 2
